31efc6dfd4a899fefdace305a740f6e5087c5415
gottong <fabian.gottong@haw-hamburg.de>
2024-04-17 20:48:37 +0200

    Aufgabe ADP01.1 Erledigt

--- src/ADP1/InplaceSuche.java
+++ src/ADP1/InplaceSuche.java
@@ -1,6 +1,7 @@
 package ADP1;
 
 import java.util.Arrays;
+import java.util.Map;
 
 public class InplaceSuche {
 
@@ -9,7 +10,12 @@ public class InplaceSuche {
 
         int[] resultAry1 = {1, 61, 89, 75, 16};
 
-        System.out.println(Arrays.toString(localMax(ary1,1)));
+
+        int[] ary3 = {99,1, 61, 89, 75, 16, 33, 89, 59, 28, 3, 3, 97, 61, 85, 47, 38, 78, 7, 6, 15};
+        int[] resultAry3 = null;
+        int radius3 = 1;
+
+        System.out.println(Arrays.toString(localMax(ary3,radius3)));
 
     }
 
@@ -38,27 +44,33 @@ public class InplaceSuche {
  * 2.2 sind alle nachbarn rechts kleiner
  * 3. vergleiche die Kleineren Nachbarn - rufe die methode für den kleineren nachbarn auf.
  */
+        // Abbruchbedingung
+        if (low > high || low == high) return null;
 
-        int currentIndex = low + (high-low)/2;
+
+        int currentIndex = low + (int)Math.ceil((high-low)/2);
         int minimumLeft = Integer.MAX_VALUE;
         int minimumRight = Integer.MAX_VALUE;
         boolean allLeftMinor = true;
         boolean allRightMinor = true;
 
         // Nachbarn Links Betrachten
-        for(int i= 0; i<radius;i++){
+        for(int i= 1; i<=radius;i++){
             if(ary[currentIndex-i]<minimumLeft) minimumLeft=ary[currentIndex-i]; // merke dir den Kleinsten Linken nachbarn.
             if(ary[currentIndex-i]>ary[currentIndex]) allLeftMinor = false; // ist auf der linkenseite eine größer?
         }
 
         //Nachbarn Rechts Betrachten
-        for(int i = 0; i<radius;i++){
+        for(int i = 1; i<=radius;i++){
             if(ary[currentIndex+i]<minimumRight) minimumRight=ary[currentIndex+i]; // merke dir den Kleinsten Rechten nachbarn.
             if(ary[currentIndex+i]>ary[currentIndex]) allRightMinor = false; // ist auf der linkenseite eine größer?
         }
 
         //Erfolgsfall: der aktuelle Index ist ein Lokales Maximum, alle nachbarn im Radius sind kleiner.
         if(allLeftMinor && allRightMinor) return Arrays.copyOfRange(ary,currentIndex-radius,currentIndex+radius+1);
+
+        //Fehlerfall: es gibt kein Locales maximum, da der verbebleibdende bereich kleiner als die nachbarschaft ist.
+        if(Math.abs(high-low)<=2*radius) return null;
 
         // Rekursiver aufruf - einer der nachbarn war größer, untersuche die hälfte des arra
         if(minimumLeft<=minimumRight) {
