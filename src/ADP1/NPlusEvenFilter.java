36bedbc7dd0c0bed0dca751fe4b57311b35ad994
gottong <fabian.gottong@haw-hamburg.de>
2024-04-18 12:33:04 +0200

    Aufgabe ADP01 Teil B 2. NEvenPlusFilter umgesetzt

---
+++ src/ADP1/NPlusEvenFilter.java
@@ -0,0 +1,27 @@
+package ADP1;
+
+import edu.princeton.cs.algs4.StdIn; // Muss algs4.jar in den Project-structures -> Modules -> Dependencies hinzufügen. Und im der Umgebungsvariablen bis zur jar verknüpfen...
+
+import java.util.InputMismatchException;
+
+
+/**
+ * Klasse, die aus einem Eingabestrom nur die
+ * positiven ganzen geraden Zahlen filtert und diese auf der Konsole ausgibt.
+ */
+public class NPlusEvenFilter {
+
+    public static void main(String[] args) {
+
+        int zahl;
+
+        while (!StdIn.isEmpty()){
+            try {
+                zahl = StdIn.readInt();
+                if(zahl>=0 && zahl%2==0) System.out.printf("%3d\n",zahl);
+            } catch (InputMismatchException ime) {
+            // Wenn die Zahl kein integer ist mache nichts. Setze einfach mit for fort.
+            }
+        }
+    }
+}
