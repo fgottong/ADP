1e9384b6aa70385d703df44542d70eb9225fd15b
gottong <fabian.gottong@haw-hamburg.de>
2024-04-24 12:34:32 +0200

    B3 Not-Rettung

--- src/Praktikum1/NumberGenerator.java
+++ src/Praktikum1/NumberGenerator.java
@@ -1,47 +1,71 @@
-package ADP1;
+package Praktikum1;
 
 import java.util.Arrays;
 import java.util.Random;
-import java.util.stream.Collectors;
 
-/**
- * eine Klasse NumberGenerator, die N Zufallszahlen (int und double
- * Werte ) in einem vorgegebenen Intervall [min,max] generiert. N, min und max sollen dem
- * Programm beim Start übergeben werden. int und double Werte sollten ungefähr gleich
- * verteilt sein. NumberGenerator gibt die Zahlen durch Leerzeichen separiert auf der Konsole
- * aus.
- * Aufruf mit : ADP1.NumberGenerator N min max
- * BSP: ADP1.NumberGenerator 10 1 5
- */
 public class NumberGenerator {
+    protected double[] ary;
+    protected Random rand;
+    protected int N;
+    protected double min;
+    protected double max;
+
+    public NumberGenerator(int n, double max) {
+        this(n, Double.MIN_VALUE, max);
+        this.ary = new double[this.N];
+        this.fillArray(n, max);
+    }
+
+    public NumberGenerator(int n, double min, double max) {
+        this.N = n;
+        this.rand = new Random();
+        this.min = min;
+        this.ary = new double[this.N];
+        this.fillArray(n, min, max);
+    }
+
     public static void main(String[] args) {
-        int N = Integer.parseInt(args[0]);
-        int min = Integer.parseInt(args[1]);
-        int max = Integer.parseInt(args[2]);
+        NumberGenerator numGen = new NumberGenerator(Integer.parseInt(args[0]), Double.parseDouble(args[1]), Double.parseDouble(args[2]));
+        System.out.println(numGen);
+    }
 
-        String[] randomNumbers = new String[N];
-        Random rand = new Random();
-
-
-        for(int i=0; i < N ; i++){
-            if(rand.nextBoolean()) {
-                randomNumbers[i]=Double.toString(rand.nextDouble(min,max+1)) ;
-            }else{
-                randomNumbers[i]=Integer.toString(rand.nextInt(min,max+1));
+    private void fillArray(int N, double max) {
+        for(int i = 0; i < N; ++i) {
+            if (this.rand.nextBoolean()) {
+                this.ary[i] = this.rand.nextDouble(max + 1.0);
+            } else {
+                this.ary[i] = (double)this.rand.nextInt((int)max + 1);
             }
         }
 
-
-//        StringBuilder outputString = new StringBuilder();
-//
-//        for(double d: randomNumbers){
-//            outputString.append( d );
-//            outputString.append(" ");
-//        }
-//        System.out.println(outputString);
-
-
-        System.out.println(String.join(" ", randomNumbers));
     }
 
+    private void fillArray(int N, double min, double max) {
+        for(int i = 0; i < N; ++i) {
+            if (this.rand.nextBoolean()) {
+                this.ary[i] = this.rand.nextDouble(min, max + 1.0);
+            } else {
+                this.ary[i] = (double)this.rand.nextInt((int)min, (int)max + 1);
+            }
+        }
+
+    }
+
+    public String toString() {
+        StringBuilder aryString = new StringBuilder();
+        double[] var2 = this.ary;
+        int var3 = var2.length;
+
+        for(int var4 = 0; var4 < var3; ++var4) {
+            double v = var2[var4];
+            aryString.append(v);
+            aryString.append(" ");
+        }
+
+        return aryString.toString().trim();
+    }
+
+    public double[] getAry() {
+        return Arrays.copyOf(this.ary, this.ary.length);
+    }
 }
