90298e5ff69320561e5d12d56bad7fc5311cf6bd
gottong <fabian.gottong@haw-hamburg.de>
2024-06-05 11:02:34 +0200

    ADP2 - Nachbesserung - Code Cleanup

--- src/Praktikum2/QuickUnionClient.java
+++ src/Praktikum2/QuickUnionClient.java
@@ -93,7 +93,10 @@ public class QuickUnionClient {
 
         System.out.printf("After union: -----------------\n[0, 1, 2, 3, 4, 5, 6, 7, 8]\n%s\n", qu_worst);
 
-
+        System.out.println("Laufzeitmessungen: ----------------------------------------");
+        // FG: 2024-06-05:
+        // Resultate werden in den Unterordner Results geschrieben.
+        // Dort liegt auch ein Jupyter/Python Notebook für die Auswertung,
         int nodesCount = 100;
         int repeatitions = 12;
         doublingTest(nodesCount,repeatitions,false);
@@ -104,8 +107,8 @@ public class QuickUnionClient {
     /**
      * Erzeugt eine Liste von Integer Paaren
      * Als Knoten für den Graphen für die Verdoppelungstest von QuickUnion.
-     * @param amountOfNodes
-     * @return
+     * @param amountOfNodes - Anzahl der Knoten die im Graphen enthalten sein sollen
+     * @return Liste der Knotenpaare, also Kanten, welche den Graphen bilden
      */
     public static List<int[]> generateNodes(int amountOfNodes) {
         Random rand = new Random(336699); // Damit immer die Selben Knoten und Kanten erzeugt werden.
@@ -129,42 +132,6 @@ public class QuickUnionClient {
      */
     public static void doublingTest(int N, int repeats, boolean compression) {
 
-//        int amountOfNodes = 10;
-//        QuickUnion qu;
-//        List<int[]> nodesList;
-//        long currentRunTime = 0;
-//        long stopTime = 0;
-//        long startTime = 0;
-//
-//        nodesList = generateNodes(amountOfNodes);
-//
-//        String output = nodesList.stream().map(x -> String.format("%2d - %2d", x[0], x[1])).collect(Collectors.joining("\n"));
-//        System.out.printf("Liste der Knoten ist : \n%s", output);
-//        System.out.println();
-//
-//
-//        // Laufzeitmessung für normalen Quick Union
-//        qu = new QuickUnion(amountOfNodes);
-//        startTime = System.nanoTime();
-//        for (int[] pair : nodesList) {
-//            qu.union(pair[0], pair[1]);
-//        }
-//        stopTime = System.nanoTime();
-//        currentRunTime = stopTime - startTime;
-//
-//        System.out.printf("Laufzeit für %d Knoten ohne Kompression: %d\n", amountOfNodes, currentRunTime);
-//
-//        // Laufzeitmessung für Kompremierenden Quick Union
-//        qu = new QuickUnion(amountOfNodes);
-//        startTime = System.nanoTime();
-//        for (int[] pair : nodesList) {
-//            qu.unionCompressed(pair[0], pair[1]);
-//        }
-//        stopTime = System.nanoTime();
-//        currentRunTime = stopTime - startTime;
-//
-//        System.out.printf("Laufzeit für %d Knoten mit Kompression: %d\n", amountOfNodes, currentRunTime);
-
         double[] times = new double[repeats];
 
         count(N,0,times,compression,false);
@@ -186,16 +153,16 @@ public class QuickUnionClient {
      *
      * @param N - Anzahl Knoten
      * @param t - Nummer des Durchlaufs
-     * @param times - Ergebniss Array der Laufzeitmessung
+     * @param times - Ergebnis Array der Laufzeitmessung
      * @param compression - true: mit Kompression, false: ohne Kompression
-     * @param verbose - true: Consolen ausgabe, false: keine Consolenausgabe
+     * @param verbose - true: Konsolenausgabe, false: keine Konsolenausgabe
      */
     public static void count(int N, int t, double[] times, boolean compression, boolean verbose) {
         QuickUnion qu;
         List<int[]> nodesList;
-        double startTime = 0;
-        double stopTime = 0;
-        double currentRunTime = 0;
+        double startTime ;
+        double stopTime ;
+        double currentRunTime;
 
         nodesList = generateNodes(N);
 
@@ -250,7 +217,7 @@ public class QuickUnionClient {
      * @param compression - true: Komprimierender Algorithmus, false: Normaler Quickunion
      */
     public static void writeResultsToCSV(int trial, int N, double time, double timeRatio, boolean compression) {
-        //Zeitstemple der Datei
+        //Zeitstempel der Datei
         SimpleDateFormat df = new SimpleDateFormat ("yyyy-MM-dd");
         String todayString = df.format(new Date ());
 
@@ -271,7 +238,8 @@ public class QuickUnionClient {
             bufferedWriter.write(trial + "," + N + "," + time + "," + timeRatio);
             bufferedWriter.newLine();
         } catch (IOException e) {
-            e.printStackTrace();
+            System.err.println("Schade - beim Schreiben der Ergebnisdatei ist ein fehler aufgetreten...");
+            System.err.println(e.getMessage());
         }
     }
 
